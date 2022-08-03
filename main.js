803a684f448fe6d9363fa4bf8a1aada0797c95e8
Victor Souza <victorssprj@gmail.com>
2022-08-03 13:24:30 -0300

    Adicionando filtro de elementos

--- main.js
+++ main.js
@@ -1,103 +1,97 @@
-/*$(".btn").click(function(){
-    var attr = $(this).attr("data-li");
+const btnsDeFiltro = document.querySelector('.navbtns'); // pega tudo que está em navbtns (que são os botões) e coloca na const btnsDeFiltro
+const categoriaDeReceita = document.querySelectorAll('.categoria'); //pega tudo que está em categoria (que são os ***tipos*** de receitas no site) e coloca na const categoriaDeReceita
 
-    $(".btn").removeClass("active");
-    $(this).addClass("active");
+btnsDeFiltro.addEventListener("click", (event) => { //evento ao clicar em um dos botões
+    if (event.target.classList.contains("btn")) { // se o alvo do evento contem a classe btn
 
-    $(".item").hide();
+        btnsDeFiltro.querySelector(".active").classList.remove("active"); //retira o active dele, modificando o highlight para o botão clicado e...
+        event.target.classList.add("active"); //...adiciona o active ao alvo que foi clicado
+        console.log("teste evento")
 
-    if(attr == "bolos"){
-        $("." + attr).show();
-    }
-    else if(attr == "doces"){
-        $("." + attr).show();
-    }
-    else if(attr == "drinks"){
-        $("." + attr).show();
-    }
-    else{
-        $(".item").show();
+
+        const filtraValor = event.target.getAttribute("data-li"); // const filtraValor pega o atributo do data-li (todos, bolos, drinks, doces)
+
+        categoriaDeReceita.forEach((categoriaDeReceita) => { //para cada categoriaDeReceita 
+
+            if (categoriaDeReceita.classList.contains(filtraValor) || filtraValor === 'todos') { // se a classe do categoriaDeReceita contém valor do filtraValor (bolos, drinks, doces) ou é estritamente igual a 'todos'
+                categoriaDeReceita.classList.remove("esconde");
+                categoriaDeReceita.classList.add("mostra");
+            }
+
+            else {
+                categoriaDeReceita.classList.remove("mostra");
+                categoriaDeReceita.classList.add("esconde");
+            }
+
+        });
     }
 });
 
-function ativafavorito() {
-    
-    
-    var Image_Id = document.getElementById('blike');
-            if (Image_Id.src.match("icone-noire-noir.png")) {
-                Image_Id.src = "like.png";
-            }
-            else {
-                Image_Id.src = "icone-noire-noir.png";
-            }
-}*/
 
 function ativafavorito1() {
-    
-    
-    var img = document.getElementById('curtir');
-            if (img.src.match("icone-noire-noir.png")) {
-                img.src = "like.png";
-            }
-            else {
-                img.src = "icone-noire-noir.png";
-            }
-}
 
+
+    var img = document.getElementById('curtir');
+    if (img.src.match("icone-noire-noir.png")) {
+        img.src = "like.png";
+    }
+    else {
+        img.src = "icone-noire-noir.png";
+    }
+}
 function ativafavorito2() {
-    
-    
+
+
     var img = document.getElementById('curtir2');
-            if (img.src.match("icone-noire-noir.png")) {
-                img.src = "like.png";
-            }
-            else {
-                img.src = "icone-noire-noir.png";
-            }
+    if (img.src.match("icone-noire-noir.png")) {
+        img.src = "like.png";
+    }
+    else {
+        img.src = "icone-noire-noir.png";
+    }
 }
 function ativafavorito3() {
-    
-    
+
+
     var img = document.getElementById('curtir3');
-            if (img.src.match("icone-noire-noir.png")) {
-                img.src = "like.png";
-            }
-            else {
-                img.src = "icone-noire-noir.png";
-            }
+    if (img.src.match("icone-noire-noir.png")) {
+        img.src = "like.png";
+    }
+    else {
+        img.src = "icone-noire-noir.png";
+    }
 }
 function ativafavorito4() {
-    
-    
+
+
     var img = document.getElementById('curtir4');
-            if (img.src.match("icone-noire-noir.png")) {
-                img.src = "like.png";
-            }
-            else {
-                img.src = "icone-noire-noir.png";
-            }
+    if (img.src.match("icone-noire-noir.png")) {
+        img.src = "like.png";
+    }
+    else {
+        img.src = "icone-noire-noir.png";
+    }
 }
 function ativafavorito5() {
-    
-    
+
+
     var img = document.getElementById('curtir5');
-            if (img.src.match("icone-noire-noir.png")) {
-                img.src = "like.png";
-            }
-            else {
-                img.src = "icone-noire-noir.png";
-            }
+    if (img.src.match("icone-noire-noir.png")) {
+        img.src = "like.png";
+    }
+    else {
+        img.src = "icone-noire-noir.png";
+    }
 }
 function ativafavorito6() {
-    
-    
-    var img = document.getElementById('curtir6');
-            if (img.src.match("icone-noire-noir.png")) {
-                img.src = "like.png";
-            }
-            else {
-                img.src = "icone-noire-noir.png";
-            }
-}
 
+
+    var img = document.getElementById('curtir6');
+    if (img.src.match("icone-noire-noir.png")) {
+        img.src = "like.png";
+    }
+    else {
+        img.src = "icone-noire-noir.png";
+    }
+}
 

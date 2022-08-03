88d5da519f1c1f33485101cf5a889449ca99a7e3
Victor Souza <victorssprj@gmail.com>
2022-08-03 10:32:02 -0300

    Primeiras alterações

---
+++ main.js
@@ -0,0 +1,103 @@
+/*$(".btn").click(function(){
+    var attr = $(this).attr("data-li");
+
+    $(".btn").removeClass("active");
+    $(this).addClass("active");
+
+    $(".item").hide();
+
+    if(attr == "bolos"){
+        $("." + attr).show();
+    }
+    else if(attr == "doces"){
+        $("." + attr).show();
+    }
+    else if(attr == "drinks"){
+        $("." + attr).show();
+    }
+    else{
+        $(".item").show();
+    }
+});
+
+function ativafavorito() {
+    
+    
+    var Image_Id = document.getElementById('blike');
+            if (Image_Id.src.match("icone-noire-noir.png")) {
+                Image_Id.src = "like.png";
+            }
+            else {
+                Image_Id.src = "icone-noire-noir.png";
+            }
+}*/
+
+function ativafavorito1() {
+    
+    
+    var img = document.getElementById('curtir');
+            if (img.src.match("icone-noire-noir.png")) {
+                img.src = "like.png";
+            }
+            else {
+                img.src = "icone-noire-noir.png";
+            }
+}
+
+function ativafavorito2() {
+    
+    
+    var img = document.getElementById('curtir2');
+            if (img.src.match("icone-noire-noir.png")) {
+                img.src = "like.png";
+            }
+            else {
+                img.src = "icone-noire-noir.png";
+            }
+}
+function ativafavorito3() {
+    
+    
+    var img = document.getElementById('curtir3');
+            if (img.src.match("icone-noire-noir.png")) {
+                img.src = "like.png";
+            }
+            else {
+                img.src = "icone-noire-noir.png";
+            }
+}
+function ativafavorito4() {
+    
+    
+    var img = document.getElementById('curtir4');
+            if (img.src.match("icone-noire-noir.png")) {
+                img.src = "like.png";
+            }
+            else {
+                img.src = "icone-noire-noir.png";
+            }
+}
+function ativafavorito5() {
+    
+    
+    var img = document.getElementById('curtir5');
+            if (img.src.match("icone-noire-noir.png")) {
+                img.src = "like.png";
+            }
+            else {
+                img.src = "icone-noire-noir.png";
+            }
+}
+function ativafavorito6() {
+    
+    
+    var img = document.getElementById('curtir6');
+            if (img.src.match("icone-noire-noir.png")) {
+                img.src = "like.png";
+            }
+            else {
+                img.src = "icone-noire-noir.png";
+            }
+}
+
+
